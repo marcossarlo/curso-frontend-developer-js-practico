165c1b237d6892db32986486f9d38d6357e4097a
marcossarlo <msl.net@gmail.com>
2023-01-16 20:51:48 -0500

    integrar <aside class=product-detail para que se despliegue al darle clic al icon ShoppingCart, tanto en la vista Desktop como en Mobile

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
-/* Función toggleDesktopMenu() para hacer que aparezca/desaparezca class="desktop-menu" */
+/* Constantes para el desktop menu */
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
+/* Constantes para el mobile-menu */
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+burguerMenu.addEventListener('click', toggleMobileMenu);
+
+/* Constantes para el product-detail */
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+menuShoppingCart.addEventListener('click', toggleCartAside);
+
+
+/* Función toggleDesktopMenu() para hacer que aparezca/desaparezca el Desktop Menu -> class="desktop-menu" */
 function toggleDesktopMenu() {
+    /* para verificar si aside esta cerrado o no */
+    const isAsideClose = aside.classList.contains('inactive');
+    if (!isAsideClose) {
+        aside.classList.add('inactive');
+    }
+
     /* quita/pone clase .inactive dependiendo de si la tiene o no */
     desktopMenu.classList.toggle('inactive');
 } 
-/* END función toggleDesktopMenu() */
 
+/* Función toggleMobileMenu() para hacer que aparezca/desaparezca el Mobile Menu -> class="mobile-menu" */
+function toggleMobileMenu() {
+    /* verificar si aside esta cerrado o no */
+    const isAsideClose = aside.classList.contains('inactive');
+    if (!isAsideClose) {
+        aside.classList.add('inactive');
+    }
+
+    /* quita/pone clase .inactive dependiendo de si la tiene o no */
+    mobileMenu.classList.toggle('inactive');
+}
+
+/* Función para hacer que aparezca/desaparezca el aside -> class="product-detail" */
+function toggleCartAside() {
+    /* verificar si .mobile-menu esta cerrado o no */
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+    if (!isMobileMenuClose) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    /* quita/pone clase .inactive dependiendo de si la tiene o no */
+    aside.classList.toggle('inactive');
+}
